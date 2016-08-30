a18e02a554b1d97a7860c2d3560c5e670fea38e6
Saurabh Jain <30197821@acsind.com>
2016-08-30 15:01:34 -0400

    Nancy Microservice Demo

---
+++ NancyFXExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyFXExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:8080";
+            HostConfiguration hostConfigs = new HostConfiguration();
+            hostConfigs.UrlReservations.CreateAutomatically = true;
+            using (var host = new NancyHost(new Uri(url), new DefaultNancyBootstrapper(), hostConfigs))
+            {
+                host.Start();
+                Console.Write("Nancy Server Listening on {0}" ,url);
+                Console.ReadLine();
+            
+            }
+        }
+    }
+}
---
+++ NancyFXExample/RootRoutes.cs
@@ -0,0 +1,74 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.ModelBinding;
+
+namespace NancyFXExample
+{
+    public class RootRoutes : NancyModule
+    {
+        public RootRoutes()
+        {
+            /*
+            Get["/"] = parameters =>
+            {
+                return "Hello World";
+            };
+            Get["jsontest"] = parameters =>
+            {
+                var test = new
+                {
+                    Name = "Peter Shaw",
+                    Twitter = "shawty_ds",
+                    Occupation = "Software Developer"
+                };
+
+                return Response.AsJson(test);
+           };
+            */
+            Get["/"] = Index;
+            Get["jsontest"] = JsonTest;
+            Get["hello/{name}"] = HelloName;
+            Post["posttest"] = PostTest;
+        }
+
+        private dynamic Index(dynamic parameters)
+        {
+            return "Hello World";
+        }
+
+        private dynamic JsonTest(dynamic parameters)
+        {
+            var test = new
+            {
+                Name = "Peter Shaw",
+                Twitter = "shawty_ds",
+                Occupation = "Software Developer"
+            };
+
+            return Response.AsJson(test);
+        }
+        private dynamic HelloName(dynamic parameters)
+        {
+            var name = parameters.name;
+
+            return String.Format("Hello there {0}", name);
+        }
+        private dynamic PostTest(dynamic parameters)
+        {
+            var myPerson = this.Bind<Person>();
+            
+            return String.Format("Hello there {0} with twitter handle {1} who works as a {2}", myPerson.Name, myPerson.Twitter, myPerson.Occupation);
+        }
+    }
+     public class Person
+   {
+      public string Name { get; set; }
+      public string Twitter { get; set; }
+      public string Occupation { get; set; }
+ 
+   }
+}
