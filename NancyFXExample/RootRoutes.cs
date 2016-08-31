0e8cfff6dac60204d4be42a2c2cd1df6b334fa16
Saurabh <itsaurabh@yahoo.com>
2016-08-31 16:48:54 -0400

    Changes Made

--- NancyFXExample/RootRoutes.cs
+++ NancyFXExample/RootRoutes.cs
@@ -37,7 +37,7 @@ namespace NancyFXExample
 
         private dynamic Index(dynamic parameters)
         {
-            return "Jai Ho" ;
+            return "Test Jenkin Integration" ;
         }
 
         private dynamic JsonTest(dynamic parameters)
